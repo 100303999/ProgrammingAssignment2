9b90b4474aa76266287f24c1f0d962b604cadd86
100303999 <100303999@alumnos.uc3m.es>
2019-07-23 10:50:15 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code creates a cached matrix object with its corresponding getter and setter functions
 
-## Write a short comment describing this function
-
+# Function that creates a pseudo vector object with getter and setter functions
 makeCacheMatrix <- function(x = matrix()) {
-
+        # Matrix inverse
+        m <- NULL
+        
+        # Sets object attributes (x <- matrix, m <- inverse)
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+                }
+        
+        # Get matrix
+        get <- function() x
+        
+        # Set matrix inverse
+        setinverse <- function(inverse) m <<- inverse
+        
+        # Get matrix inverse
+        getinverse <- function() m
+        
+        # Return list of all object values
+        list(set = set, get = set, setinverse = setinverse, getinverse = getinverse)        
 }
 
 
-## Write a short comment describing this function
-
+# Calculate and set matrix x's inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        
+        if(!is.null(m)) {
+                message("Getting cached data")
+                return(m)
+        }
+        
+        else {
+                data <- x$get()
+                m <- solve(data, ...)
+                x$setinverse(m)
+                m
+        }
+}
 }
